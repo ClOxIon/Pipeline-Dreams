1ad680c006220939fd83c8c862ec3858e2b3f195
Jinseo Park <parkjinseo2@naver.com>
2020-03-19 11:27:11 -0500

    soul system added

--- Assets/Scripts/Entity/EntityLoot.cs
+++ Assets/Scripts/Entity/EntityLoot.cs
@@ -1,17 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 namespace PipelineDreams {
     public class EntityLoot : MonoBehaviour {
         Entity entity;
         ItemContainerPlayer PI;
+        [SerializeField] EntityDataContainer entityDataContainer;
         private void Awake() {
             
             entity = GetComponent<Entity>();
             entity.OnEntityDeath += EntityLoot_OnEntityDeath;
-            PI = (ItemContainerPlayer)FindObjectOfType(typeof(ItemContainerPlayer));
         }
 
         private void EntityLoot_OnEntityDeath(Entity obj) {
+
+            PI = entityDataContainer.FindEntitiesOfType(EntityType.PLAYER).FirstOrDefault()?.GetComponent<EntityItem>().ItemContainer;
             if (PI == null)
             {
                 Debug.LogWarning("PlayerItem is not Assigned!");
---
+++ Assets/UI/Scripts/SoulUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PipelineDreams {
+    public class SoulUI : MonoBehaviour
+    {
+        [SerializeField] Text SoulText;
+        [SerializeField] Entity Player;
+        // Start is called before the first frame update
+    void Awake()
+        {
+            Player.OnParamChange += Player_OnParamChange;
+            SoulText.text = "Souls: 0";
+        }
+
+        private void Player_OnParamChange(string arg1, float arg2)
+        {
+            if (arg1 != "Souls") return;
+            SoulText.text = "Souls: " + arg2;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
